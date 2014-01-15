f0c797d321dbe08250da97071f6df4b418b61ad2
mardibiase <mardibiase@gmail.com>
2014-01-15 11:22:13 +0100

    Refactoring della classe MainActivity

--- Veloxdroid/src/com/veloxdroid/veloxdroid/MainActivity.java
+++ Veloxdroid/src/com/veloxdroid/veloxdroid/MainActivity.java
@@ -1,25 +1,14 @@
 package com.veloxdroid.veloxdroid;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import com.veloxdroid.utils.DownloadTask;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.PowerManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,8 +21,6 @@ public class MainActivity extends Activity {
 	private static String file_url = "http://10.0.2.2:8080/VDServer/download";
 	private ProgressDialog mProgressDialog;
 
-
-	 	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,126 +30,121 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		//se l'utente è già registrato ed ha già fatto login
-		//direttamente passa nella schermata di "navigazione" 
-		//altrimenti: checkFirstTimeUsage
-		checkFirstTimeUsage();
-		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		boolean condition = settings.getBoolean("first_time", false);
-		if (condition == false) checkIsLoggedIn();
+		// check wheter it's the first time you use the app
+		// if the user is logged in, checkLogin() returns true
+		// else returns false
+		if (checkLogin()) {
+			// TODO - Check if the user has selected to automatically download
+			// autovelox files
+
+			// automatically fires download file - we do not pass from
+			// downloadFiles
+			// but instead we directly instantiate a new DownloadTask and
+			// execute it
+			final DownloadTask downloadTask = new DownloadTask(
+					MainActivity.this);
+			downloadTask.execute(file_url);
+
+			Toast toast = Toast.makeText(getApplicationContext(),
+					"Bentornato in Veloxdroid", 5);
+			toast.show();
+
+			// redirect to NavigationActivity
+			Intent intent = new Intent(this, NavigationActivity.class);
+			startActivity(intent);
+		} else {
+			Toast toast = Toast.makeText(getApplicationContext(),
+					"Benvenuto in Veloxdroid", 5);
+			toast.show();
+		}
 	}
 
+	// Inflate the menu; this adds items to the action bar if it is present.
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
+	// fire this method when select Settings from the option menu on the top
+	// right of the GUI
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item){
-		
-		switch(item.getItemId()){
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
 		case R.id.action_settings:
 			Intent intent = new Intent(this, SettingsActivity.class);
 			startActivity(intent);
 			return true;
-			default:
-				return super.onOptionsItemSelected(item);
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		
-	}
-	
 
-	// checks if the app is starting for the first time
-	private void checkFirstTimeUsage() {		 
-		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		if (settings.getBoolean("first_time", true)) {
-			// the app is being launched for first time, do something
-			Log.d("Comments", "First time");
-			// first time task
-			Toast toast = Toast.makeText(getApplicationContext(),
-					"Benvenuto in Veloxdroid", 5);
-			toast.show();
-			// record the fact that the app has been started at least once
-			settings.edit().putBoolean("first_time", false).commit();
-			
-		}else{
-			
-			// execute this when the downloader must be fired
-			final DownloadTask downloadTask = new DownloadTask(MainActivity.this);
-			downloadTask.execute(file_url);
-			
-			Toast toast = Toast.makeText(getApplicationContext(), "Bentornato in Veloxdroid", 5);
-			toast.show();
-			
-			
-			//Intent intent = new  Intent(this, NavigationActivity.class);
-			//startActivity(intent);
-		}		
 	}
-	
-	private void checkIsLoggedIn(){
-		SharedPreferences settings = getSharedPreferences(MainActivity.PREFS_NAME, 0);
+
+	// static reference to checkLogin - because we need a context from where
+	// recover getSharedPrefereces in the private method below
+	public static boolean checkLogin(Context context) {
+		MainActivity ma = (MainActivity) context;
+		return ma.checkLogin();
+	}
+
+	// checks whether the user is logged with his email
+	private boolean checkLogin() {
+		SharedPreferences settings = getSharedPreferences(
+				MainActivity.PREFS_NAME, 0);
 		String s = settings.getString("eMail", "");
-		
-		Log.d("Email", s);
-		
-		Intent intent;
-		if (!s.equalsIgnoreCase("")){
-//			intent = new  Intent(this, LoginActivity.class);
-//		else
-			intent = new  Intent(this, NavigationActivity.class);
-		
-		startActivity(intent);
-		}
+		Log.d("checkLogin", s);
+		if (s.equalsIgnoreCase(""))
+			return false;
+		else
+			return true;
 	}
 
+	// method that downloads the file of the autoveloxes
+	// fires when clicking on the button Download on the  GUI
 	public void downloadFiles(View view) {
-		Toast toast = Toast.makeText(getApplicationContext(), "Clicked on the button!", 5);
+		Toast toast = Toast.makeText(getApplicationContext(), "Download file",
+				5);
 		toast.show();
-
 		// execute this when the downloader must be fired
+		// we have to do in an AsyncTask separately because Android doesn't
+		// allow
+		// to do this in a standard activity
 		final DownloadTask downloadTask = new DownloadTask(MainActivity.this);
 		downloadTask.execute(file_url);
-			
-//		mProgressDialog
-//				.setOnCancelListener(new DialogInterface.OnCancelListener() {
-//					@Override
-//					public void onCancel(DialogInterface dialog) {
-//						downloadTask.cancel(true);
-//					}
-//				});
+
+		// TODO - controllare perch� questa mProgressDialog dava dei problemi
+		// mProgressDialog
+		// .setOnCancelListener(new DialogInterface.OnCancelListener() {
+		// @Override
+		// public void onCancel(DialogInterface dialog) {
+		// downloadTask.cancel(true);
+		// }
+		// });
 
 	}
-	
-	public void doLogin(View view){
-		int requestCode = 99; //hardcoded
+
+	// listener on the Login button of the GUI
+	public void doLogin(View view) {
+		int requestCode = 99; // hardcoded
 		Intent intent = new Intent(this, LoginActivity.class);
 		startActivityForResult(intent, requestCode);
 	}
-	
+
 	@Override
-	protected void onActivityResult(int aRequestCode, int aResultCode, Intent aData){
-		switch(aRequestCode){
-		case 99: { //hardcoded in doLogin
+	protected void onActivityResult(int aRequestCode, int aResultCode,
+			Intent aData) {
+		switch (aRequestCode) {
+		case 99: { // hardcoded in doLogin
 			String result = aData.getData().toString();
-			Toast toast = Toast.makeText(getApplicationContext(),
-					result, 10);
+			Toast toast = Toast.makeText(getApplicationContext(), result, 10);
 			toast.show();
 			Intent intent = new Intent(this, NavigationActivity.class);
 			startActivity(intent);
 			break;
-			}
+		}
 		}
 	}
-	
-	
-	
-//	public void doRegister(View view){
-//		Intent intent = new Intent(this, RegisterActivity.class);
-//		startActivity(intent);
-//	}
-	
 
 }
